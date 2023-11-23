17fed440aa012b7007b537844b36484344903134
Diego Gutierrez <ibox.org@gmail.com>
2023-11-23 19:14:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv-flow').config()
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.end('Hello Node! Genup \nSECRET KEY = ' + process.env.SECRET_KEY);
+  res.end('Hello Node! Genup 2.1 \nSECRET KEY = ' + process.env.SECRET_KEY);
 });
 
 server.listen(port, () => {
